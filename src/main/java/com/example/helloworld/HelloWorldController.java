f7ea5f184534948320fc3cb28bfc1c0f1f14fcd1
sujendar <sujendarreddy345@gmail.com>
2023-09-07 13:52:42 +0530

    Add files via upload

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,8 +1,12 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 @RestController
 public class HelloWorldController {
@@ -12,10 +16,14 @@ public class HelloWorldController {
 	  
 	  @Value("${topicKey}")
 	  public String topicKey;
+	  @Autowired
+	  @Qualifier(value = "KeyVaultAutoconfiguredClient")
+	  private  KeyVaultClient keyVaultClient;
 	 
     @GetMapping("/hello")
 	public String get() {
-		return "HelloWorld "+topicEndpoint;
+    	 KeyVaultSecret keyVaultSecret = keyVaultClient.getSecret("DemoSecret");
+		return "HelloWorld "+keyVaultSecret;
 		
 	}
 }
