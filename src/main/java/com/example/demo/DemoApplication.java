8e7cb635b9f8e0918bf77a84bbf520e166479856
sujendar <sujendarreddy345@gmail.com>
2023-09-08 13:30:20 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("get")
+	  public String get() {
+	    return connectionString;
+	  }
+
+	  @Value("${Demourl}")
+	  private String connectionString;
+
+	  /*public void run(String... varl) throws Exception {
+	    System.out.println(String.format("\nConnection String stored in Azure Key Vault:\n%s\n",connectionString));
+	  } */ 
+}
