a5e8f0f4118bf47f41608c34044335b41ada5716
sujendar <sujendarreddy345@gmail.com>
2023-09-08 08:28:16 +0530

    Add files via upload

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -7,23 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.azure.security.keyvault.secrets.SecretClient;
 
 @SpringBootApplication
-public class HelloworldApplication implements CommandLineRunner{
+public class HelloworldApplication {
 	
-	 private  final SecretClient secretClient;
 
-	    public HelloworldApplication(SecretClient secretClient) {
-	        this.secretClient = secretClient;
-	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		  System.out.println("h2url: " + secretClient.getSecret("Demourl").getValue());
-		
-	}
+	
 
 }
